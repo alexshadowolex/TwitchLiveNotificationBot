6442ba4546b22b075136edbafb7ccc6928226883
Alex <>
2023-07-29 16:53:21 +0200

    Project Setup

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
+    id("org.jetbrains.compose") version "1.4.1"
+}
+
+group = "me.alex"
+version = "1.0.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+
+    implementation(kotlin("script-runtime"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlin.sourceSets.all {
+        languageSettings.apply {
+            optIn("kotlin.RequiresOptIn")
+            optIn("kotlin.time.ExperimentalTime")
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "TwitchLiveNotificationBot"
+            packageVersion = version.toString()
+        }
+    }
+}
